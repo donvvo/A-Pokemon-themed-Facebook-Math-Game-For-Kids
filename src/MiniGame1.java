d205bc3281277c8e7e2e4a3b1dd3111b31ad94b3
Sandeep Soni <ssoni6@uwo.ca>
2013-11-16 08:14:34 -0500

    ignore this commit

--- src/MiniGame1.java
+++ src/MiniGame1.java
@@ -14,7 +14,7 @@ public class MiniGame1 extends JApplet {
 		getContentPane().setLayout(null);
 		
 		JLabel BackGround1 = new JLabel("BackGround1");
-		BackGround1.setIcon(new ImageIcon(MiniGame1.class.getResource("/Icons/Background1.png")));
+		BackGround1.setIcon(new ImageIcon(MiniGame1.class.getResource("/Icons/BackgroundLevel1.png")));
 		BackGround1.setBounds(0, 0, 800, 400);
 		getContentPane().add(BackGround1);
 		//edit to test pushes
