f25d47464b6adca81ac6315d5d2ff0bc9c0013c7
vvo3@uwo.ca <vietcavo@Vietcas-MacBook-Pro.local>
2013-11-21 12:04:28 -0500

    GU update
    -admin mode complete

--- src/adminhome.java
+++ src/adminhome.java
@@ -12,6 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class adminhome extends JApplet {
@@ -25,53 +27,56 @@ public class adminhome extends JApplet {
 		setBounds(0,0,800,600);
 		
 		
-		JButton btnChangeAge = new JButton("Change Age");
+		JButton btnChangeAge = new JButton("");
 		btnChangeAge.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
 		btnChangeAge.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/Buttons/change-age.jpg")));
-		btnChangeAge.setBounds(79, 524, 222, 29);
+		btnChangeAge.setBounds(147, 533, 226, 34);
 		getContentPane().add(btnChangeAge);
 		
-		JButton btnDelete = new JButton("Delete");
+		JButton btnDelete = new JButton("");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDelete.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/Buttons/delete.jpg")));
 		btnDelete.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnDelete.setBounds(355, 200, 117, 29);
+		btnDelete.setBounds(335, 186, 147, 29);
 		getContentPane().add(btnDelete);
 		
-		JButton btnAdd = new JButton("Add");
+		JButton btnAdd = new JButton("");
+		btnAdd.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/Buttons/add.jpg")));
 		btnAdd.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnAdd.setBounds(213, 200, 117, 29);
+		btnAdd.setBounds(132, 235, 104, 41);
 		getContentPane().add(btnAdd);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(249, 89, 52, 27);
-		getContentPane().add(comboBox);
+		JComboBox timeLimit = new JComboBox();
+		timeLimit.setBounds(104, 147, 64, 27);
+		getContentPane().add(timeLimit);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(334, 89, 52, 27);
-		getContentPane().add(comboBox_1);
+		JComboBox mistakes = new JComboBox();
+		mistakes.setBounds(357, 147, 52, 27);
+		getContentPane().add(mistakes);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(155, 89, 52, 27);
-		getContentPane().add(comboBox_2);
+		JComboBox childSelection = new JComboBox();
+		childSelection.setBounds(39, 76, 117, 29);
+		getContentPane().add(childSelection);
 		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setBounds(79, 89, 52, 27);
-		getContentPane().add(comboBox_3);
-		
-		JButton btnRevert = new JButton("Revert");
-		btnRevert.setBounds(625, 235, 117, 29);
-		getContentPane().add(btnRevert);
+		JButton btnBack = new JButton("");
+		btnBack.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/Buttons/back.jpg")));
+		btnBack.setBounds(604, 449, 98, 29);
+		getContentPane().add(btnBack);
 		
 		JButton btnFullStats = new JButton("Full Stats");
 		btnFullStats.addMouseListener(new MouseAdapter() {
@@ -80,48 +85,39 @@ public class adminhome extends JApplet {
 			}
 		});
 		btnFullStats.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/Buttons/view-stats.jpg")));
-		btnFullStats.setBounds(453, 521, 224, 34);
+		btnFullStats.setBounds(460, 521, 217, 34);
 		getContentPane().add(btnFullStats);
 		
 		JLabel lblNewLabel = new JLabel("Scores");
-		lblNewLabel.setBounds(484, 70, 104, 179);
+		lblNewLabel.setBounds(429, 159, 104, 179);
 		getContentPane().add(lblNewLabel);
 		
 		
 		JLabel lblNewLabel_1 = new JLabel("Top Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High ScoresTop Scores/High Scores");
 		lblNewLabel_1.setBackground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(219, 113, 253, 309);
+		lblNewLabel_1.setBounds(313, 109, 253, 309);
 		getContentPane().add(lblNewLabel_1);
 		
 		table = new JTable();
 		table.setBackground(new Color(51, 0, 153));
 		table.setBounds(68, 474, 183, -80);
 		getContentPane().add(table);
-		
-		JButton btnChild = new JButton("Child 1");
-		btnChild.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
-		btnChild.setBounds(35, 146, 117, 29);
-		getContentPane().add(btnChild);
-		
-			
-			JLabel lblAdminChild = new JLabel("New label");
-			lblAdminChild.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-1.jpg")));
-			lblAdminChild.setBounds(0, 0, 800, 600);
-			getContentPane().add(lblAdminChild);
-			
-			JLabel lblAdminMain = new JLabel("New label");
-			lblAdminMain.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-edit.jpg")));
-			lblAdminMain.setBounds(0, 0, 800, 600);
-			getContentPane().add(lblAdminMain);
-			
-			JLabel lblAdminScore = new JLabel("New label");
-			lblAdminScore.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-scores.jpg")));
-			lblAdminScore.setBounds(0, 0, 800, 600);
-			getContentPane().add(lblAdminScore);
+					
+					JLabel lblAdminScore = new JLabel("New label");
+					lblAdminScore.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-scores.jpg")));
+					lblAdminScore.setBounds(0, 0, 800, 600);
+					getContentPane().add(lblAdminScore);
+					
+						
+						JLabel lblAdminChild = new JLabel("New label");
+						lblAdminChild.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-1.jpg")));
+						lblAdminChild.setBounds(0, 0, 800, 600);
+						getContentPane().add(lblAdminChild);
+						
+						JLabel lblAdminMain = new JLabel("New label");
+						lblAdminMain.setIcon(new ImageIcon(adminhome.class.getResource("/Icons/BackGrounds/admin-edit.jpg")));
+						lblAdminMain.setBounds(0, 0, 800, 600);
+						getContentPane().add(lblAdminMain);
 
 		
 		
